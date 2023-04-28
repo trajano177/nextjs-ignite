6b086a95e89897c620abb1fcd5da5182d6ce7d66
Matheus Trajano <matheustrajanotel@gmail.com>
2023-04-28 14:27:01 -0300

    att

---
+++ src/pages/styles/index.ts
@@ -0,0 +1,17 @@
+import { createStitches } from '@stitches/react'
+
+export const {
+    config,
+    styled,
+    css,
+    keyframes,
+    getCssText,
+    theme,
+    createTheme
+} = createStitches({
+    theme: {
+        colors: {
+            rockeseat: '#n0257e6',
+        }
+    }
+})
