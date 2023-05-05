2dc6bc854a05a7dbc901e06bcd03bd283d7671da
Matheus Trajano <matheustrajanotel@gmail.com>
2023-05-05 14:14:03 -0300

    att

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,21 @@
 import { styled } from "@stitches/react"
+import { HomeConatiner, Product } from "./styles/pages/home"
+import Image from "next/image"
+import camiseta1 from "../assets/camisetas/1.png"
+import camiseta2 from "../assets/camisetas/2.png"
+import camiseta3 from "../assets/camisetas/3.png"
 
 
-const Button = styled('button', {
-  backgroundColor: '$green500',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
 
-  span: {
-    fontWeight: 'bold',
-  },
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  },
-})
 
 export default function Home() {
   return (
-    <Button>
-      <span>Tester</span>
-    </Button>
+    <HomeConatiner>
+      <Product>
+        <Image src={camiseta1} alt=""/>
+        <Image src={camiseta2} alt=""/>
+        <Image src={camiseta3} alt=""/>
+      </Product>
+    </HomeConatiner>
   )
 }
---
+++ src/pages/styles/pages/home.ts
@@ -0,0 +1,9 @@
+import { styled } from "..";
+
+export const HomeConatiner = styled('main', {
+
+});
+
+export const Product = styled('div', {
+    
+})
