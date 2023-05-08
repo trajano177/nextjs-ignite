5c07afc30bc0a192a8832eda5b139c70480eb893
Matheus Trajano <matheustrajanotel@gmail.com>
2023-05-08 14:26:04 -0300

    att

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,7 +12,7 @@ export default function App({ Component, pageProps }: AppProps){
   return ( 
     <Container>
       <Header>
-          <Image src="logoImg" alt="#"/>
+          <Image src={logoImg} alt="" />
           
       </Header>
       <Component {...pageProps} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,8 @@
-import { styled } from "@stitches/react"
 import { HomeConatiner, Product } from "./styles/pages/home"
-import Image from "next/image"
-import camiseta1 from "../assets/camisetas/1.png"
-import camiseta2 from "../assets/camisetas/2.png"
-import camiseta3 from "../assets/camisetas/3.png"
+import Image from "next/image";
+import camiseta1 from "../pages/assets/camisetas/1.png";
+import camiseta2 from "../pages/assets/camisetas/2.png"
+import camiseta3 from "../pages/assets/camisetas/3.png";
 
 
 
@@ -12,10 +11,32 @@ export default function Home() {
   return (
     <HomeConatiner>
       <Product>
-        <Image src={camiseta1} alt=""/>
-        <Image src={camiseta2} alt=""/>
-        <Image src={camiseta3} alt=""/>
+        <Image src={camiseta1} width={520} height={480} alt="#"/>
+        
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
       </Product>
+
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="#"/>
+        
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta3} width={520} height={480} alt="#"/>
+        
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      
     </HomeConatiner>
   )
 }
