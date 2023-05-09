e7c0ee617751c664cfd9e150ff4bd65bc34ba0e1
Matheus Trajano <matheustrajanotel@gmail.com>
2023-05-09 14:09:18 -0300

    attt

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,13 +4,22 @@ import camiseta1 from "../pages/assets/camisetas/1.png";
 import camiseta2 from "../pages/assets/camisetas/2.png"
 import camiseta3 from "../pages/assets/camisetas/3.png";
 
+import {useKeenSlider} from "keen-slider/react"
+import 'keen-slider/keen-slider.min.css'
+
 
 
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48
+    }
+  })
   return (
-    <HomeConatiner>
-      <Product>
+    <HomeConatiner ref={sliderRef} className="keen-slider">
+      <Product  className="keen-slider__slide">
         <Image src={camiseta1} width={520} height={480} alt="#"/>
         
         <footer>
@@ -19,7 +28,7 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={camiseta2} width={520} height={480} alt="#"/>
         
         <footer>
@@ -28,7 +37,7 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={camiseta3} width={520} height={480} alt="#"/>
         
         <footer>
