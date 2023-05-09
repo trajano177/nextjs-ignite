07c4dfd118430290ee289822013db08fa2289477
matheus trajano <matheustrajanotel@gmail.com>
2023-05-08 21:36:33 -0300

    att

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,14 +6,13 @@ import Image from "next/image";
 
 
 globalStyles();
-export default function App({ Component, pageProps }: AppProps){
- 
-  
-  return ( 
+export default function App({ Component, pageProps }: AppProps) {
+
+
+  return (
     <Container>
       <Header>
-          <Image src={logoImg} alt="" />
-          
+        <Image src={logoImg} alt="" />
       </Header>
       <Component {...pageProps} />
     </Container>
