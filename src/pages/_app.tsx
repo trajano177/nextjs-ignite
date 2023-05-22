cd15a1b31e40827937228c3fbce2482a410b7254
Matheus Trajano <matheustrajanotel@gmail.com>
2023-05-22 14:13:51 -0300

    att

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
+        <h1>dd</h1>
         <Image src={logoImg} alt="" />
       </Header>
       <Component {...pageProps} />
