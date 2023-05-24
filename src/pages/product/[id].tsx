0b4e1f8b279a585a8d12485835b42480c921018c
Matheus Trajano <matheustrajanotel@gmail.com>
2023-05-24 14:01:30 -0300

    att

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,23 @@
 import { useRouter } from 'next/router'
+import { ImageContainer, ProductContainer, ProductDetails } from '../styles/pages/product'
 
 export default function Products() {
   // Query vem de dentro do useRouter
   const { query } = useRouter()
   return (
-    <h1>produdos:{JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>camixa x</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque voluptatum deserunt harum ea laudantium a culpa aut quia illo, animi esse quos cupiditate voluptatem, dolore dolorem deleniti repudiandae. Cum, totam?</p>
+       <button>Compre agora </button>
+      </ProductDetails>
+
+    </ProductContainer>
   )
 }
