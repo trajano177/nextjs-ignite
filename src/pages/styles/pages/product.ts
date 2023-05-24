ffe4399fbfe4129b051f65c25013be2fac85f60b
Matheus Trajano <matheustrajanotel@gmail.com>
2023-05-24 14:12:50 -0300

    att

--- src/pages/styles/pages/product.ts
+++ src/pages/styles/pages/product.ts
@@ -5,17 +5,17 @@ export const ProductContainer = styled('main', {
     gridTemplateColumns: '1fr 1fr',
     alignItems: 'strech',
     gap: '4rem',
-    maxWidth: '1100',
+    maxWidth: 1180,
     margin: '0 auto',
 
 })
 
 export const ImageContainer = styled('div', {
     width: '100%',
-    maxWidth: '576',
+    maxWidth: 576,
     background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
     borderRadius: 8,
-    height: 'clac()',
+    height: 656,
     padding:' 0.25rem',
 
     display: 'flex',
@@ -29,5 +29,30 @@ export const ImageContainer = styled('div', {
 })
 
 export const ProductDetails = styled('main', {
+    display: 'flex',
+    flexDirection: 'column',
 
+    h1: {
+        fontSize: '$2xl',
+        color: '$gray300'
+    },
+
+    span: {
+        marginTop: '1rem',
+        display: 'flex',
+        fontSize: '$2xl',
+        color:'$green300'
+    },
+
+    p: {
+        marginTop: '2.5rem',
+        fontSize: '$md',
+        lineHeight: 1.6,
+        color: '$gray300'
+    },
+
+    button: {
+        marginTop: 'auto',
+        
+    }
 })
