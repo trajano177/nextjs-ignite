a8cbdb7491d5a90a117e7f7dcf7dda8a14f774e5
Matheus Trajano <matheustrajanotel@gmail.com>
2023-05-25 14:08:00 -0300

    att

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,20 @@
 import { useRouter } from 'next/router'
 import { ImageContainer, ProductContainer, ProductDetails } from '../styles/pages/product'
+import { GetStaticProps } from 'next'
+import Stripe from 'stripe';
+import { stripe } from '../lib/stripe';
 
-export default function Products() {
+interface Productprops {
+  product: {
+    id: string;
+    name: string;
+    imageURL: string;
+    price: string;
+    description: string;
+  }
+}
+
+export default function Products({product}: Productprops) {
   // Query vem de dentro do useRouter
   const { query } = useRouter()
   return (
@@ -11,13 +24,41 @@ export default function Products() {
       </ImageContainer>
 
       <ProductDetails>
-        <h1>camixa x</h1>
-        <span>R$ 79,90</span>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
 
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque voluptatum deserunt harum ea laudantium a culpa aut quia illo, animi esse quos cupiditate voluptatem, dolore dolorem deleniti repudiandae. Cum, totam?</p>
+        <p>{product.description}</p>
        <button>Compre agora </button>
       </ProductDetails>
 
     </ProductContainer>
   )
+}
+
+export const getStaticProps: GetStaticProps<any, {id: string}> = async ({ params }) => {
+  const productId = params.id;
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price']
+  })
+
+  const price = product.default_price as Stripe.Price;
+  const unitAmount = price.unit_amount ?? 0;
+ 
+ 
+  return {
+    props: {
+      product: {
+          id: product.id,
+          name: product.name,
+          imageURL: product.images[0],
+          price: new Intl.NumberFormat('pt-br', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(unitAmount / 100),
+          description: product.description
+      }
+    },
+    revalidade: 60*60 * 1 
+  }
 }
