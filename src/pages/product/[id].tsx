f15fe7080a48cd11db43ce8247d63be468cb2250
Matheus Trajano <matheustrajanotel@gmail.com>
2023-05-27 07:56:34 -0300

    att

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -43,11 +43,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
     params: { id },
   }));
 
-  return {
-    paths,
-    fallback: false,
-  };
-};
+
 
 export const getStaticProps: GetStaticProps<any, {id: string}> = async ({ params}) => {
   const productId = params?.id || '';
