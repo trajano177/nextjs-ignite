4c59f27dbd5cc90593aa51aeadb97eff902c4f3f
Matheus Trajano <matheustrajanotel@gmail.com>
2023-06-05 13:47:35 -0300

    att git commit -m att git commit -m att

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,17 @@
+import Link from "next/link";
+import {ImageConatiner, SuccessConatiner } from "./styles/pages/success";
+
 export default function Success() {
   return (
-    <h1>success</h1>
+    <SuccessConatiner>
+      <h1>Compra efetuada com sucesso!</h1>
+    <ImageConatiner>
+
+    </ImageConatiner>
+    <p>Uhuul <strong>Diego Fernandes</strong>, sua <strong> Camiseta Beyond the Limits</strong> já está a caminho da sua casa. </p>
+
+    <Link href={"/"}>Voltar ao catálogo!</Link>
+
+    </SuccessConatiner>
   )
 }
