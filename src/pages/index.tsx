f6b1f84cd3bf71b970b6fc3503bff1fe86be1092
Matheus Trajano <matheustrajanotel@gmail.com>
2023-06-07 14:46:17 -0300

    att

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,12 @@ interface HomeProps {
 
 
 export default function Home({ products }: HomeProps) {
-  
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 4,
+      spacing: 48
+    }
+  })
 
   return (
     <HomeConatiner ref={sliderRef} className="keen-slider">
