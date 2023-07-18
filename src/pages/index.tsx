916b60ad4b8de95bf08f1f78d5bc34231f896b24
Matheus Trajano <matheustrajanotel@gmail.com>
2023-07-18 11:30:47 -0300

    att

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -48,7 +48,7 @@ export default function Home({ products }: HomeProps) {
     </HomeConatiner>
   )
 }
-console.log(Home)
+
 
 export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
